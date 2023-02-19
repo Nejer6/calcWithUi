fe4e2234b145b0ff40f9bf679425fe336be341b8
Nejer <Nejer6@gmail.com>
2023-02-20 01:41:13 +0300

    fix romanToInt

--- src/jvmMain/kotlin/CalcWithRoman.kt
+++ src/jvmMain/kotlin/CalcWithRoman.kt
@@ -2,9 +2,11 @@ import extensions.RomanNumberFormatException
 import extensions.romanToInt
 import extensions.toRomanNumber
 
-class FormatException: Exception("the format of the mathematical operation does not satisfy the task - two operands and one operator (+, -, /, *)")
-class DifferentNumberSystemException: Exception("different number systems are used simultaneously")
-class OperandIsNotNumberException: Exception("one ore more operands is not a number")
+class FormatException :
+    Exception("the format of the mathematical operation does not satisfy the task - two operands and one operator (+, -, /, *)")
+
+class DifferentNumberSystemException : Exception("different number systems are used simultaneously")
+class OperandIsNotNumberException : Exception("one ore more operands is not a number")
 
 fun calculate(str: String): String {
     val splitStr = str.split(' ').filter { it != "" }
@@ -15,40 +17,66 @@ fun calculate(str: String): String {
     }
 
     val operator = splitStr[1]
+    val numStr1 = splitStr[0]
+    val numStr2 = splitStr[2]
 
     try {
-        val num1 = splitStr[0].toInt()
+        val num1 = numStr1.romanToInt()
         try {
-            val num2 = splitStr[2].toInt()
-            return execute(num1, num2, operator).toString()
-        } catch (e: NumberFormatException) {
+            val num2 = numStr2.romanToInt()
+            return execute(num1, num2, operator).toRomanNumber()
+        } catch (e: RomanNumberFormatException) { //num2 - не roman
             try {
-                splitStr[2].romanToInt()
+                numStr2.toInt()
                 throw DifferentNumberSystemException()
-            } catch (e: RomanNumberFormatException) {
+            } catch (e: NumberFormatException) { //num2 - не int
                 throw OperandIsNotNumberException()
             }
         }
-    } catch (e : NumberFormatException) {
+    } catch (e: RomanNumberFormatException) { //num1 - не roman
         try {
-            val num1 = splitStr[0].romanToInt()
+            val num1 = numStr1.toInt()
             try {
-                val num2 = splitStr[2].romanToInt()
-                val result = execute(num1, num2, operator)
-                return result.toRomanNumber()
-            } catch (e: RomanNumberFormatException) {
+                val num2 = numStr2.toInt()
+                return execute(num1, num2, operator).toString()
+            } catch (e: NumberFormatException) { //num2 - не int
                 try {
-                    splitStr[2].toInt()
+                    numStr2.romanToInt()
                     throw DifferentNumberSystemException()
-                } catch (e: NumberFormatException) {
-                    println(e.message)
+                } catch (e: RomanNumberFormatException) { //num2 - не roman
                     throw OperandIsNotNumberException()
                 }
             }
-        } catch (e: RomanNumberFormatException) {
+        } catch (e: NumberFormatException) { //num1 - не int
             throw OperandIsNotNumberException()
         }
     }
+//    try {
+//        val num1 = splitStr[0].toInt()
+//        try {
+//            val num2 = splitStr[2].toInt()
+//            return execute(num1, num2, operator).toString()
+//        } catch (e: NumberFormatException) {
+//            splitStr[2].romanToInt()
+//            throw DifferentNumberSystemException()
+//        }
+//    } catch (e: NumberFormatException) {
+//
+//        val num1 = splitStr[0].romanToInt()
+//        try {
+//            val num2 = splitStr[2].romanToInt()
+//            val result = execute(num1, num2, operator)
+//            return result.toRomanNumber()
+//        } catch (e: RomanNumberFormatException) {
+//            try {
+//                splitStr[2].toInt()
+//                throw DifferentNumberSystemException()
+//            } catch (e: NumberFormatException) {
+//                throw OperandIsNotNumberException()
+//            }
+//        }
+//
+//    }
 }
 
 private fun execute(num1: Int, num2: Int, operation: String): Int {
--- src/jvmMain/kotlin/extensions/RomanNumberExt.kt
+++ src/jvmMain/kotlin/extensions/RomanNumberExt.kt
@@ -1,45 +1,64 @@
 package extensions
 
+import kotlin.math.pow
+
+const val MAX_ROMAN_NUMBER = 3999
 val romanCharset = mapOf('I' to 1, 'V' to 5, 'X' to 10, 'L' to 50, 'C' to 100, 'D' to 500, 'M' to 1000)
 val numbersByRuleOfSubtraction = mapOf("IV" to 4, "IX" to 9, "XL" to 40, "XC" to 90, "CD" to 400, "CM" to 900)
+val romanNumbers = listOf(
+    "MMM" to 3000,
+    "MM" to 2000,
+    "M" to 1000,
+    "CM" to 900, //
+    "D" to 500,
+    "CD" to 400,
+    "CCC" to 300,
+    "CC" to 200,
+    "C" to 100,
+    "XC" to 90, //
+    "L" to 50,
+    "XL" to 40,
+    "XXX" to 30,
+    "XX" to 20,
+    "X" to 10,
+    "IX" to 9, //
+    "V" to 5,
+    "IV" to 4,
+    "III" to 3,
+    "II" to 2,
+    "I" to 1
+)
 
-class RomanNumberFormatException(message: String): Exception(message)
+class RomanNumberFormatException(message: String) : Exception(message)
 
 fun String.romanToInt(): Int {
-    val str = this.uppercase()
+    var str = this.uppercase()
 
-    str.forEach {
-        if (!romanCharset.contains(it)) {
-            throw RomanNumberFormatException("The string has characters not related to the Roman notation")
-        }
-    }
+    var minNum = Int.MAX_VALUE
+    var number = 0
 
-    val nums = mutableListOf<Int>()
+    romanNumbers.forEach { pair ->
+        if (str.startsWith(pair.first)) {
+            if (minNum > pair.second) {
+                minNum = when {
+                    pair.second % 10 == 0 -> 10.0.pow(pair.second.toString().length.toDouble()).toInt()
+                    pair.second % 9 == 0 -> pair.second / 9
+                    pair.second % 5 == 0 -> pair.second * 8 / 10
+                    pair.second % 4 == 0 -> pair.second / 4
+                    else -> 0
+                }
 
-    var i = 0
-    while (i < str.length) {
-        if (i + 1 < str.length) {
-            val numberByRuleOfSubtraction = numbersByRuleOfSubtraction[str.slice(i .. i + 1)]
-            if (numberByRuleOfSubtraction != null) {
-                nums.add(numberByRuleOfSubtraction)
-                i += 2
-                continue
+                number += pair.second
+                str = str.removePrefix(pair.first)
+            } else {
+                throw RomanNumberFormatException("incorrect entry of the Roman number")
             }
         }
-        nums.add(romanCharset[str[i]]!!)
-        i++
     }
 
-    var minNum = nums.first()
-    var number = nums.first()
-    for (i in 1 until nums.size) {
-        if (nums[i] > minNum) {
-            throw RomanNumberFormatException("incorrect entry of the Roman number")
-        }
-        minNum = nums[i]
-        number += nums[i]
+    if (str != "") {
+        throw RomanNumberFormatException("incorrect entry of the Roman number")
     }
-
     return number
 }
 
@@ -51,6 +70,10 @@ fun Int.toRomanNumber(): String {
         throw Exception("there are no zero or negative numbers in the Roman system")
     }
 
+    if (num > MAX_ROMAN_NUMBER) {
+        throw Exception("Roman numbers cannot be greater than 3999")
+    }
+
     while (num != 0) {
         var max1 = 0
         romanCharset.values.forEach {
@@ -60,7 +83,7 @@ fun Int.toRomanNumber(): String {
         }
         var max2 = 0
         numbersByRuleOfSubtraction.values.forEach {
-            if (it in (max2 + 1) .. num) {
+            if (it in (max2 + 1)..num) {
                 max2 = it
             }
         }
